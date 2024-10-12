c3ab2681ea89f8983253c959e09c72430a191ae1
John Shoemaker <shoemjoh@gmail.com>
2024-10-12 14:45:19 -0400

    working i o lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    new_file_name = str(file_name) + ".txt"
+    with open(new_file_name, mode="w", encoding='utf-8') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    new_file_name = str(file_name) + ".txt"
+    with open(new_file_name, mode="a", encoding='utf-8') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    new_file_name = str(file_name) + ".txt"
+    with open(new_file_name, encoding='utf-8') as file:
+       content = file.read()
+    return content
